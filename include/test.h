9948269f9d162f7187ec3c734e530b8ea95fc304
Max Morgunov <max@pop-os.localdomain>
2021-09-13 13:28:35 +0300

    Added docs to test.h

--- include/test.h
+++ include/test.h
@@ -1,15 +1,28 @@
 #include <math.h>
 
+//! Encapsulates the set of values needed for testing
 typedef struct testCase {
-    size_t id;
-    double a;
-    double b;
-    double c;
-    size_t nRoots;
-    double x1;
-    double x2;
+    size_t id;     /**< Test id */
+    double a;      /**< a-coefficient */
+    double b;      /**< b-coefficient */
+    double c;      /**< c-coefficient */
+    size_t nRoots; /**< Expected number of roots */
+    double x1;     /**< Expected first root */
+    double x2;     /**< Expected second root */
 } tCase;
 
+//------------------------------------------------------------ 
+//! Checks result of solveQuadratic function against test values
+//!
+//! @param[in]  test    Struct, encapsulating test info, equation coefficients and expected values
+//!
+//! @return 1 if test is successful, 0 otherwise
+//------------------------------------------------------------ 
 int checkTestCase(tCase test);
 
+//------------------------------------------------------------ 
+//! Parses test cases from a file and runs them with checkTestCase
+//!
+//! @param[in]  path    Path to file, containing test cases 
+//------------------------------------------------------------ 
 void runTests(const char* path);
